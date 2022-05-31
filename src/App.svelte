7e8945a64eb98da88bd050d18dd6a09813be5ee6
Ramon Antonio <netsurfer2132@gmail.com>
2022-05-31 01:17:20 -0400

    Create proof-of-concept card for set thumbnails.

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
+	import SetThumbnail from "./lib/components/SetThumbnail.svelte";
+
 	export let name: string;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<SetThumbnail />
 </main>
+
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
+	/>
+</svelte:head>
 
 <style>
 	main {
---
+++ src/lib/components/SetThumbnail.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import Card, {
+    Content,
+    PrimaryAction,
+    Media,
+    MediaContent,
+  } from "@smui/card";
+
+  let clicked = 0;
+</script>
+
+<div class="card-container">
+  <Card>
+    <PrimaryAction on:click={() => clicked++} style="padding:0 20px;">
+      <Content>
+        <b>winner's pools</b><br />
+        sgg user 1<br />
+        vs<br />
+        sgg user 2
+      </Content>
+    </PrimaryAction>
+  </Card>
+</div>
+
+<pre class="status">Clicked: {clicked}</pre>
+
+<style>
+  .card-container {
+    width: fit-content;
+  }
+</style>
